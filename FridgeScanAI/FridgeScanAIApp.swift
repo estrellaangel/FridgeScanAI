8a9fa9765ebb5a8a180139369d757498f447d9da
estrellaangel <ella34138@gmail.com>
2025-04-07 15:26:37 -0700

    Initial Commit

---
+++ FridgeScanAI/FridgeScanAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeScanAIApp.swift
+//  FridgeScanAI
+//
+//  Created by Estrella Angel on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FridgeScanAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
