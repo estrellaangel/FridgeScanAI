fee55189b1862ea13b237d1034ace101ddda0218
sdfarias03 <150176835+sdfarias03@users.noreply.github.com>
2025-04-19 11:26:42 -0700

    Update SettingsView.swift
    Adding shell of settings page

--- FridgeScanAI/Views/SettingsView.swift
+++ FridgeScanAI/Views/SettingsView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text("Where settings will go")
-        //https://www.youtube.com/watch?v=WqPoFzVrLj8
-    }
-    
-    
-    
-    
+        VStack(spacing: 20) {
+            VStack {
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.gray)
+                Text("User Name")
+                    .font(.headline)
+                    .foregroundColor(.secondary)
+            }
+            .padding(.top, 40)
+
+            //TODO : Implement functionality in settings buttons
+            Form {
+                Section {
+                    Button("Past Scans") {}
+                    Button("Weekly Grocery Items") {}
+                    Button("App Info") {}
+                    Button("Help & Support") {}
+                    Button("Log Out") {}
+                        .foregroundColor(.red)
+                }
+            }
+        }
+    } 
 }
 
 #Preview {
